9c8fbea8f2670526e0001fa422446618169cc14f
Joseph Collantes <josephcollantes65@gmail.com>
2025-03-28 08:02:08 +0800

    initial commiit

--- my-portfolio/tailwind.config.js
+++ my-portfolio/tailwind.config.js
@@ -1,15 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Correct paths
-    theme: {
-      extend: {
-        colors: {
-          black: "#000000",
-          dirtyWhite: "#f9f6f1",
-          lightGray: "#d3d3d3",
-        },
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"], // Ensure it scans these files
+  theme: {
+    extend: {
+      colors: {
+        black: "#000000",
+        dirtyWhite: "#f9f6f1",
+        lightGray: "#d3d3d3",
       },
     },
-    plugins: [],
-  };
-  
+  },
+  plugins: [],
+};
